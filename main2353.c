011f2e0127b6c4fae7457e84f0f6dd0774218675
mark <mark@voodoofish.net>
2012-07-04 14:50:48 -0700

    Updated code to now use case select. All 7 options are available and do simple blink patterns. Cleaned up and removed some code. Moved functions supportUtils.h

--- main2353.c
+++ main2353.c
@@ -3,11 +3,28 @@
 //When pressed, led1 will go off and the LED2 will blink X times. LED1 will come
 //on again.
 
+/*LED blink status
+ * LED's are on pins P2.0, P2.1 and P2.2
+ * 001 -> 1 -> 0x1
+ * 010 -> 2 -> 0x2
+ * 011 -> 3 -> 0x3
+ * 100 -> 4 -> 0x4
+ * 101 -> 5 ->
+ * 110 -> 6 ->
+ * 111 -> 7 ->
+ * 
+ * Buttons are on P2.3 and P2.5
+ */
+
 #include "msp430g2353.h"
-//#include "msp430x20x2.h"
+#include "supportUtils.h"
+
+unsigned char S1 = 0; //button1/action  Also used as reset button when switch is enabled
+unsigned char S2 = 0; //button2/selector
 int toggler = 0x0;
 int blinky = 0x0;
-int j;
+int j=0;
+unsigned char counter = 0;
 volatile unsigned int i; 
 void delay(unsigned int ms);
 
@@ -15,33 +32,94 @@ void delay(unsigned int ms);
 void main(void)
 {
 WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
-P2OUT &= ~0x07; //Set P2.0 and P1.6 output to low.
-P2DIR |= 0x07; // Set P2.0 and P1.6 to output direction
-//P1OUT |=0x40; //sets P1.6 high
-P2IE |= 0x08; // P2.3 interrupt enabled
-P2IES |= 0x08; // P2.3 Hi/lo edge
-P2IFG &= ~0x08; // P2.3 IFG cleared
+P2OUT &= ~0x07; //Set P2.0 - P2.2 output to low.
+P2DIR |= 0x07; // Set P2.0 - P2.2 output direction
+
+P2IE |= 0x28; // P2.3 and P2.5 interrupt enabled
+P2IES |= 0x28; // P2.3 and P2.5  Hi/lo edge
+P2IFG &= ~0x28; // P2.3 and P2.5  IFG cleared
 
 while(1){
 	if (blinky ==1){
-		if (toggler == 0)
+	
+	switch(S2)
+	{
+	case 1 :
+		if (S1 == 0){//if S1 is set, skip the setpins blink and just go to the actual code we'll run.
+			setpins(0x1);}
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 2 :
+		if (S1 == 0){	
+			setpins(0x2);}	
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 3 :
+		if (S1 == 0)	
+			setpins(0x3);
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 4 :
+		if (S1 == 0)	
+			setpins(S2);
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 5 :
+		if (S1 == 0)	
+			setpins(S2);
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 6 :
+		if (S1 == 0)	
+			setpins(S2);
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+	case 7 :
+		if (S1 == 0)	
+			setpins(S2);
+		else
+			{blinkfun();
+			S2 = 0;}
+		break;
+		
+	default :
+	counter = 0;
+	}
+	
+		/*	
+	    if (toggler == 0)
 		{
-		while((BIT3 & P2IN) == 0x0){
-		P2OUT |=0x01;
-		delay(500);
-		P2OUT &= ~0x01;
-		delay(500);
-		}
+			while((BIT3 & P2IN) == 0x0){
+			P2OUT |=0x01;
+			delay(200);
+			P2OUT &= ~0x01;
+			delay(200);
+			}
 		toggler = 1;
 		}
 		else
 		{P2OUT |=0x03;
-		delay(500);
+		delay(200);
 		P2OUT &= ~0x03;
 		toggler = 0;
 		}
+		*/
 	}
 	blinky = 0;
+	//S2 = 0;
+	S1 = 0;
 _BIS_SR(LPM4_bits + GIE); // Enter LPM4 w/interrupt
 }
 }
@@ -50,27 +128,19 @@ _BIS_SR(LPM4_bits + GIE); // Enter LPM4 w/interrupt
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-/*
-P1OUT &= ~0x40; //Turn off P1.6
 
-for(j=0;j<5;j++){ //on->off-->on->off-->on   with 5 and <= you get 3 blinks
-    P1OUT ^=0x01; //sets P1.0 high
-    i = 10000; // SW Delay
-    do i--;
-    while (i != 0);
-	}
-	
-P1OUT &= ~0x01; //turn off the led after it lights for the last blink
-P1OUT |=0x40; //sets P1.6 high
-*/
+if((0x08 & P2IFG)==0x08) 
+{ if(S2>=7)
+	{S2=0;}
+	else 
+	{S2 +=1;}
+}
+
+if((P2IFG & 0x20)==0x20) 
+{S1 = 1;}
 blinky = 1;
-P2IFG &= ~0x08; // P1.3 IFG cleared
+P2IFG &= ~0x08; // P2.3 IFG cleared
+P2IFG &= ~0x20; // P2.5 IFG cleared
 _low_power_mode_off_on_exit();
 }
-void delay(unsigned int ms)
-{
- while (ms--)
-    {
-        __delay_cycles(1000); // set for 16Mhz change it to 1000 for 1 Mhz
-    }
-}
+
